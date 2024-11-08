46b8064c9ea5cf94bbcb1a1942445f726b1b55c2
UT01317 <UT01317@UNICOMTIC24>
2024-11-08 17:23:56 +0530

    Bike Entity Crud

--- BikeRental_System3/BikeRental_System3/Services/BikeService.cs
+++ BikeRental_System3/BikeRental_System3/Services/BikeService.cs
@@ -3,6 +3,7 @@ using BikeRental_System3.DTOs.Response;
 using BikeRental_System3.IRepository;
 using BikeRental_System3.IService;
 using BikeRental_System3.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BikeRental_System3.Services
 {
@@ -36,6 +37,71 @@ namespace BikeRental_System3.Services
 
             };
             return res;
+        }
+
+        public async Task<List<BikeResponse>> GetAllBikes()
+        {
+            var data = await _bikeRepository.GetAllBikes();
+            var list = data.Select(x => new BikeResponse
+            {
+                Id=x.Id,
+                Brand = x.Brand,
+                Type = x.Type,
+                Model = x.Model,
+                RatePerHour = x.RatePerHour
+            }).ToList();
+            return list;
+        }
+
+        public async Task<BikeResponse> GetBikeById(int Id)
+        {
+                var data = await _bikeRepository.GetBikeById(Id);
+                var res = new BikeResponse
+                {
+                    Id = data.Id,
+                    Brand = data.Brand,
+                    Type = data.Type,
+                    Model = data.Model,
+                    RatePerHour = data.RatePerHour
+                };
+                return res;
+        }
+
+        public async Task<BikeResponse> UpdateBike(int Id, BikeRequest bikeRequest)
+        {
+            var get = await _bikeRepository.GetBikeById(Id);
+            get.Brand = bikeRequest.Brand;
+            get.Type = bikeRequest.Type;
+            get.Model = bikeRequest.Model;
+            get.RatePerHour = bikeRequest.RatePerHour;
+
+            var data = await _bikeRepository.UpdateBike(get);
+            var res = new BikeResponse
+            {
+                Brand = data.Brand,
+                Type = data.Type,
+                Model = data.Model,
+                RatePerHour = data.RatePerHour
+            };
+            return res;
+        }
+
+        public async Task<string> DeleteBike(int Id)
+        {
+            var get = await _bikeRepository.GetBikeById(Id);
+           var data = await _bikeRepository.DeleteBike(get);
+
+            if (data == null)
+            {
+                throw new NotFoundException($"Bike with ID {Id} was not found.");
+            }
+         
+            return "Successfully Deleted";
+        }
+
+        public class NotFoundException : Exception
+        {
+            public NotFoundException(string message) : base(message) { }
         }
 
     }
